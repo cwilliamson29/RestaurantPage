7e281e87952e3d77e8fa415df001b3da31b25fc7
Chris Williamson <chriswill0629@gmail.com>
2021-04-23 04:32:59 -0400

    added 2 modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import pageLoad from './modules/page-load.js';
 import homeLoad from './modules/home.js';
-import aboutLoad from './modules/about.js'
+import aboutLoad from './modules/about.js';
+import menuLoad from './modules/menu.js';
+import contactLoad from './modules/contact.js'
 
 
 initialize();
@@ -8,9 +10,13 @@ initialize();
 function buttonListeners() {
     const home = document.getElementById('homeBtn');
     const about = document.getElementById('aboutBtn');
+    const menu = document.getElementById('menuBtn');
+    const contact = document.getElementById('contactBtn')
 
     home.addEventListener('click', homeLoad);
     about.addEventListener('click', aboutLoad);
+    menu.addEventListener('click', menuLoad)
+    contact.addEventListener('click', contactLoad)
 }
 
 
---
+++ src/modules/contact.js
@@ -0,0 +1,51 @@
+function setActive() {
+    const navButtons = ['homeBtn', 'aboutBtn', 'menuBtn', 'contactBtn'];
+
+    for (let i = 0; i < navButtons.length;) {
+        document.getElementById(navButtons[i]).classList.remove('active');
+        document.getElementById(navButtons[i]).classList.add('inActive')
+            ++i
+    }
+
+    const contact = document.getElementById('contactBtn');
+    contact.classList.remove('inactive')
+    contact.classList.add('active');
+}
+
+function createFooter() {
+    const footer = document.createElement('footer');
+    footer.setAttribute('id', 'footer');
+
+    const footerP = document.createElement('p');
+    footerP.innerText = "Copyright © 2011 Devine Delicacies. All rights reserved."
+    footer.appendChild(footerP)
+
+    const footerP1 = document.createElement('p');
+    footerP1.innerText = "This is not an actual resturant. Please do not try to reach to this company. They do no exist. This website was created as a project for 'The Odin Project' as part of a programming course."
+    footer.appendChild(footerP1);
+
+    return footer
+}
+
+function removeAll(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+
+function contactLoad() {
+    setActive();
+
+    const butCont = document.getElementById('but-content');
+    removeAll(butCont);
+
+
+    const footer = createFooter();
+    butCont.appendChild(footer);
+
+    return butCont;
+}
+
+export default contactLoad
